18d8dee302a905dc5a47f5315166333cca3ee7c4
DavidPMCR <alleriaysebastian@gmail.com>
2025-02-04 20:09:48 -0600

    Fix bcrypt deployment issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 // Definir el puerto dinámicamente
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 // Crear una sola instancia de express
 const app = express();
